aa1ae90ffe681c24947288f06188f9b2c64f1dc3
Sreejith <38622608+sreejith-vu@users.noreply.github.com>
2019-07-19 01:55:46 +0530

    Create hba_prod_demostrate.sh

---
+++ hba_prod_demostrate.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+kubectl apply -f guestbook/redis-master-deployment.yaml -n production
+kubectl apply -f guestbook/redis-master-service.yaml -n production
+kubectl apply -f guestbook/redis-slave-deployment.yaml -n production
+kubectl apply -f guestbook/redis-slave-service.yaml -n production
+kubectl apply -f guestbook/frontend-deployment.yaml -n production
+
+kubectl apply -f guestbook/frontend-production-service.yaml -n production
+kubectl apply -f production-guestbook-ingress.yaml -n production
+
+kubectl autoscale deployment frontend --cpu-percent=10 --min=1 --max=10 -n production
+
+PROD_LB_PUB_IP=$(kubectl get svc -n production |grep LoadBalancer |awk '{print $4}')
+
+sed -i "" "s/LB_IP/$PROD_LB_PUB_IP/g" load-generator/production-load-generator.yaml
+
+kubectl apply -f load-generator/production-load-generator.yaml -n staging
+
+echo "Waiting for pod scaling"
+
+REPLICA=1
+while [[ $REPLICA -le 3 ]]
+do
+  kubectl get hpa -n production
+  kubectl get pods -n production
+
+  if [[ $REPLICA -ge 2 ]]
+  then
+    echo "Pod auto scaled on production namespace"
+    kubectl get hpa -n production
+
+    echo "Pod scaled successfully. Terminating load generator to reduce load"
+    kubectl delete -f load-generator/staging-load-generator.yaml -n production
+
+    if [[ $? -eq 0 ]]
+    then
+      echo "Deleted load generator"
+    else
+      echo "Error while deleting pod/yaml"
+    fi
+
+    while [[ $REPLICA -ge 1 ]]
+    do
+      echo "Waiting for pod to scale down"
+      kubectl get hpa -n production
+      kubectl get pods -n production
+      if [[ $REPLICA -eq 1  ]]
+      then
+        kubectl get hpa -n production
+        echo "Terminated pods as load is normal. Exiting from scipt"
+        exit 0
+      fi
+    echo "Next check after 10 sec"
+    sleep 10
+    REPLICA=$(kubectl get hpa -n production |grep frontend |awk '{print $6}')
+   done
+
+  fi
+  echo "Next check after 10 sec"
+  sleep 10
+  REPLICA=$(kubectl get hpa -n production |grep frontend |awk '{print $6}')
+done
