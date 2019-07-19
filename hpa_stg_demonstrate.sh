d795814dd199a76d78da0d9e5565a43af21ed98a
Sreejith <38622608+sreejith-vu@users.noreply.github.com>
2019-07-19 18:09:52 +0530

    Update hpa_stg_demonstrate.sh

--- hpa_stg_demonstrate.sh
+++ hpa_stg_demonstrate.sh
@@ -64,10 +64,10 @@ do
         echo "Terminated pods as load is normal. Exiting from scipt"
         exit 0
       fi
-    echo "Next check after 10 sec"
-    sleep 10
-    REPLICA=$(kubectl get hpa -n staging |grep frontend |awk '{print $6}')
-   done
+      echo "Next check after 10 sec"
+      sleep 10
+      REPLICA=$(kubectl get hpa -n staging |grep frontend |awk '{print $6}')
+    done
 
   fi
   echo "Next check after 10 sec"
