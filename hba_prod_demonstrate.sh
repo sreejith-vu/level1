df2eb20a4d4420cdd57c859a6b0145d5ebc56a45
Sreejith <38622608+sreejith-vu@users.noreply.github.com>
2019-07-19 18:08:43 +0530

    Update hba_prod_demonstrate.sh

--- hba_prod_demonstrate.sh
+++ hba_prod_demonstrate.sh
@@ -63,10 +63,10 @@ do
         echo "Terminated pods as load is normal. Exiting from scipt"
         exit 0
       fi
-    echo "Next check after 10 sec"
-    sleep 10
-    REPLICA=$(kubectl get hpa -n production |grep frontend |awk '{print $6}')
-   done
+      echo "Next check after 10 sec"
+      sleep 10
+      REPLICA=$(kubectl get hpa -n production |grep frontend |awk '{print $6}')
+    done
 
   fi
   echo "Next check after 10 sec"
